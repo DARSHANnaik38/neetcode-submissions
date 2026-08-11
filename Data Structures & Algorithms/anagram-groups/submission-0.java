a63e583cae6fefd5467f3233f233a1d341beecc1
Darshan <135335398+DARSHANnaik38@users.noreply.github.com>
2026-08-11 16:01:41 +0530

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<String> list = new ArrayList<>();
+        Map<String , List<String>> map = new HashMap<>();
+
+        for(String str : strs) {
+            char[] each = str.toCharArray();
+            Arrays.sort(each);
+            String sorted = new String(each);
+            if(!map.containsKey(sorted)) {
+                map.put(sorted , new ArrayList<>());
+            }
+
+            map.get(sorted).add(str); 
+        }
+        return new ArrayList<>(map.values());
+    }
+}
